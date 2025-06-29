f3fc37f414d0ede74c488b953ccee89f10d6ceb4
a-dtya <adithyavinod1943@gmail.com>
2025-06-29 18:44:49 +0530

    #2 added authentication page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
 import ClerkProviderWithRoutes from './auth/ClerkProviderWithRoutes'
 import { Routes, Route } from 'react-router-dom'
+import AuthenticationPage from './auth/AuthenticationPage'
+import Layout from './layout/Layout'
+
 function App() {
   return (
     <ClerkProviderWithRoutes>
       <Routes>
+        <Route path="/sign-in/*" element={<AuthenticationPage />} />
+        <Route path="/sign-up/*" element={<AuthenticationPage />} />
+        <Route element={<Layout/>}>
+          {/* <Route path="/" element={<Entry />} /> */}
+          {/* <Route path="/history" element={<History />} /> */}
+        </Route>
       </Routes>
     </ClerkProviderWithRoutes>
   )
