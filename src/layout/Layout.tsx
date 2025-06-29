23a6e5369bf4392e128e795fc12d77c2e241c92e
a-dtya <adithyavinod1943@gmail.com>
2025-06-29 23:21:41 +0530

    #1 implemented navbar in layout component

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,31 @@
 // generate a sample layout tsx
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, Navigate } from "react-router-dom";
+import {SignedIn, SignedOut, UserButton} from "@clerk/clerk-react"
+import { Box, Heading } from "@chakra-ui/react";
 
+// create a navbar component in layout
 function Layout() {
     return (
-        <Outlet />
+        <>
+        <SignedIn>
+            {/* add a title of the application, along with links to history and create a new entry */}
+            <Box display="flex" justifyContent="space-between" alignItems="center" p={4}>
+                <Heading size="lg">Reflecto</Heading>
+                <Box display="flex" gap={4} alignItems="center">
+                    <Link to="/">Create Entry</Link>
+                    <Link to="/history">History</Link>
+                    <UserButton />
+                </Box>
+            </Box>
+        </SignedIn>
+        <SignedOut>
+            {/* redirect without tab switch */}
+            <Navigate to="/sign-in" replace/>
+        </SignedOut>
+        <SignedIn>
+            <Outlet />
+        </SignedIn>
+        </>
     )
 }
 
